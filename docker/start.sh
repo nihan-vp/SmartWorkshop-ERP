1ed195e5dbeeed37652ad1fa15b1f3f871455bb6
nihan-vp <suhaimalivpsuhaim@gmail.com>
2026-06-04 16:25:46 +0530

    Fix wait loop in start.sh to connect to MySQL host without dbname to avoid early connection failure if DB does not exist yet

--- docker/start.sh
+++ docker/start.sh
@@ -86,7 +86,7 @@ RETRY=0
 until php -r "
     try {
         \$pdo = new PDO(
-            'mysql:host=${DB_HOST};port=${DB_PORT};dbname=${DB_DATABASE}',
+            'mysql:host=${DB_HOST};port=${DB_PORT}',
             '${DB_USERNAME}',
             '${DB_PASSWORD}',
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
